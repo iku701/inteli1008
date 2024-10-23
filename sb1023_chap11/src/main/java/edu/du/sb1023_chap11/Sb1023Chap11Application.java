f6bbae9365b86f85606575677f13c372c5279630
iku701 <iku701@naver.com>
2024-10-23 19:44:53 +0900

    1023commit

---
+++ sb1023_chap11/src/main/java/edu/du/sb1023_chap11/Sb1023Chap11Application.java
@@ -0,0 +1,13 @@
+package edu.du.sb1023_chap11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1023Chap11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb1023Chap11Application.class, args);
+    }
+
+}
